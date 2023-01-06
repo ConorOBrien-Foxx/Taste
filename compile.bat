c310d2af0e25339163b039514a302d6e7b0dab17
ConorOBrien-Foxx <conorobrien4god@gmail.com>
2023-01-05 23:05:57 -0500

    tidy compile targets

--- compile.bat
+++ compile.bat
@@ -2,9 +2,9 @@
 
 REM compile webpage
 ECHO Making webapp...
-elm make --output taste.js src\Main.elm
+elm make --output comp\taste.js src\Main.elm
 IF %ERRORLEVEL% NEQ 0 EXIT /B
 
 REM compile node.js
 ECHO Making node.js version...
-elm make --output taste-compiled.node.js src\NodeOp.elm
+elm make --output comp\taste-compiled.node.js src\NodeOp.elm
